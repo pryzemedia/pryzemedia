e9d5aba42e656bf0e6ef1dce7c9875034676ca86
Jeff Price <jeffrey.price@alionscience.com>
2023-09-29 14:45:49 -0500

    Initial files added - Jeff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./components/header/header.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
